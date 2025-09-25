a2e192345baef3853164d809ca934d8c74804c9e
Celestia <nazarshejkin@gmail.com>
2025-09-25 23:20:44 +0300

    Add files via upload

---
+++ ООП/LR2/Lab2_zavd2/Lab1_zavd3/CopyTicketForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Lab1_zavd3
+{
+    partial class CopyTicketForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Enabled = false;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Arial Narrow", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(114, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Зробити копію";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 30);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(77, 27);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // CopyTicketForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(260, 81);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Name = "CopyTicketForm";
+            Text = "CopyTicketForm";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ ООП/LR2/Lab2_zavd2/Lab1_zavd3/Ticket.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1_Zavd3
+{
+    internal class Ticket
+    {
+        //Test2
+
+
+        //--------Властивості----------
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NameAttraction { get; set; }
+        public int Age { get; set; }
+        public DateTime DateTime { get; set; }
+        //--------Конструктори----------
+        public Ticket() // конструктор за замовчуванням
+        {
+            FirstName = "Назар";
+            LastName = "Шейкін";
+            NameAttraction = "Freefall tower";
+            Age = 18;
+            DateTime = new DateTime(2000, 4, 1, 0, 0, 0); 
+        }
+        public Ticket(string firstName, string lastName, string nameAttraction, int year, DateTime dateTime) // з параметрами
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            NameAttraction = nameAttraction;
+            Age = year;
+            DateTime= dateTime;
+        }
+        public Ticket(Ticket other) // конструктор копіювання
+        {
+            FirstName = other.FirstName;
+            LastName = other.LastName;
+            NameAttraction = other.NameAttraction;
+            Age = other.Age;
+            DateTime= other.DateTime;
+        }
+       
+
+        //--------Перевантаження----------
+
+        // == і != (порівняння по всіх полях)
+        public static bool operator ==(Ticket t1, Ticket t2)
+        {
+            if (ReferenceEquals(t1, t2)) return true;
+            if ((object)t1 == null || (object)t2 == null) return false;
+
+            return t1.FirstName == t2.FirstName &&
+                   t1.LastName == t2.LastName &&
+                   t1.NameAttraction == t2.NameAttraction &&
+                   t1.Age == t2.Age &&
+                   t1.DateTime == t2.DateTime;
+        }
+        public static bool operator !=(Ticket t1, Ticket t2) => !(t1 == t2);
+
+        // > і < (порівняння за віком)
+        public static bool operator >(Ticket t1, Ticket t2) => t1.Age > t2.Age;
+        public static bool operator <(Ticket t1, Ticket t2) => t1.Age < t2.Age;
+
+        // Додавання (зміщення дати на кілька днів)
+        public static Ticket operator +(Ticket t, int days)
+        {
+            return new Ticket(t.FirstName, t.LastName, t.NameAttraction, t.Age, t.DateTime.AddDays(days));
+        }
+
+        // ToString() для гарного виводу
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, {Age} років, Атракціон: {NameAttraction}, Дата: {DateTime}";
+        }
+
+        // Щоб не сварився компілятор, треба перевизначити Equals і GetHashCode
+        public override bool Equals(object obj)
+        {
+            if (obj is Ticket other) return this == other;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstName, LastName, NameAttraction, Age, DateTime);
+        }
+    }
+}
